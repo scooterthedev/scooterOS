54395e188f98f4da18d620dfd18e4addd1f5ed21
Scooter <info@talkufy.com>
2024-12-22 10:50:35 -0500

    Added start for a custom compiler

---
+++ compiler/include/compiler.h
@@ -0,0 +1,44 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+    TOKEN_EOF,
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_STRING,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_SLASH,
+    TOKEN_EQUALS,
+    TOKEN_SEMICOLON,
+    TOKEN_LPAREN,
+    TOEKN_RPAREN,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_KEYWORD
+} token_type_t;
+
+typedef struct {
+    token_type_t type;
+    char* value;
+    size_t line;
+    size_t collum;
+} token_t;
+
+typedef struct {
+    char* source;
+    size_t position;
+    size_t line;
+    size_t collum;
+} lexer_t;
+
+void compiler_init(void);
+lexer_t* lexer_create(char* source);
+token_t* lexer_next_token(lexer_t* lexer);
+void lexer_destroy(lexer_t* lexer);
+
+#endif
